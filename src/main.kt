49d42d928eb2ed82c1ffab79066412c8478ef32a
Lucy-Karimi <karimiluccy@gmail.com>
2022-02-26 17:16:03 +0300

    initial commit

---
+++ src/main.kt
@@ -0,0 +1,24 @@
+fun main(){
+var school = "akirachix"
+   var char = school[0]
+    var char1 = school[2]
+    var char2 = school[3]
+    var result = "$char$char1$char2"
+println(result)
+    var y= "Lucy"
+    var x= "22"
+    println("Hi, my name is $y and I am $x years old")
+    var carType = "Merceedez"
+    println(carType.length)
+    var name = "Lucy"
+   var outcome = if (name == "Lucy"){
+        "That's me"
+
+    }else{
+        "I don't know who that is"
+    }
+println(outcome)
+}
+fun combination(){
+
+}
